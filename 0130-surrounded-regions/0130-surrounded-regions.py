505fafc74985b093ea1dc4ff92733e84bd2a1616
Deekshaa Balachandran <bde23ece@cmrit.ac.in>
2026-06-29 09:38:07 +0530

    Time: 3 ms (84.21%), Space: 22.4 MB (39.72%) - LeetHub

---
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -0,0 +1,41 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        if not board:
+            return
+
+        rows = len(board)
+        cols = len(board[0])
+
+        def dfs(r, c):
+            if r < 0 or c < 0 or r >= rows or c >= cols or board[r][c] != "O":
+                return
+
+            board[r][c] = "T"   # temporary mark
+
+            dfs(r + 1, c)
+            dfs(r - 1, c)
+            dfs(r, c + 1)
+            dfs(r, c - 1)
+
+        # Mark border-connected O's
+        for i in range(rows):
+            dfs(i, 0)
+            dfs(i, cols - 1)
+
+        for j in range(cols):
+            dfs(0, j)
+            dfs(rows - 1, j)
+
+        # Flip surrounded O -> X
+        # Convert T -> O
+        for i in range(rows):
+            for j in range(cols):
+
+                if board[i][j] == "O":
+                    board[i][j] = "X"
+
+                elif board[i][j] == "T":
+                    board[i][j] = "O"

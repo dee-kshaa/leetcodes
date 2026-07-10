e0b72fd0cec6275fbe0be9dbf3ca3879eac69088
Deekshaa Balachandran <bde23ece@cmrit.ac.in>
2026-07-10 23:21:40 +0530

    Time: 0 ms (100%), Space: 19.5 MB (12.36%) - LeetHub

--- 0065-valid-number/0065-valid-number.py
+++ 0065-valid-number/0065-valid-number.py
@@ -1,6 +1,27 @@
 class Solution:
     def isNumber(self, s: str) -> bool:
-        try:
-            return (float(s) or int(float(s)) == 0) and not ('n'in s)
-        except:
-            return(False)
+        digit = ex = dot = False
+
+        for i, c in enumerate(s):
+            if c.isdigit():
+                digit = True
+            
+            elif c in '+-':
+                if not (i == 0 or (s[i - 1]) in 'Ee'):
+                    return False
+            
+            elif c in 'eE':
+                if ex or not digit:
+                    return False
+                ex = True
+                digit = False
+            
+            elif c == '.':
+                if dot or ex:
+                    return False
+                dot = True
+            else:
+                return False
+        
+        return digit
+            

d9192eb827de9c63c2226388c9bb45ddfd9a4f1c
Deekshaa Balachandran <bde23ece@cmrit.ac.in>
2026-09-02 22:35:49 +0530

    Time: 31 ms (1.01%), Space: 19.2 MB (47.47%) - LeetHub

--- 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.py
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.py
@@ -1,4 +1,21 @@
 class Solution:
     def uniformArray(self, nums1: list[int]) -> bool:
-        return True
-        
+        n = len(nums1)
+        nums2 = [None] * n
+
+        def construct(p):
+            for i in range(n):
+                if nums1[i] % 2 == p:
+                    nums2[i] = nums1[i]
+                else:
+                    for j in range(n):
+                        if i == j:
+                            continue
+                        if (nums1[i] - nums1[j]) % 2 == p:
+                            nums2[i] = nums1[i] - nums1[j]
+
+            return not (any(nums2) == 0)
+
+        if construct(0) or construct(1):
+            return True
+        return False
